d9fea76585a0ca59750b116c90ed10e87c0e0bc3
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-05-30 12:59:54 +0530

    Update cloneall.bat

--- cloneall.bat
+++ cloneall.bat
@@ -1,2 +1,7 @@
 gh repo clone keshavsoft/AdminApi
 gh repo clone keshavsoft/JSONDataSupply
+gh repo clone keshavsoft/KStaticFiles
+gh repo clone keshavsoft/JsonApi
+gh repo clone keshavsoft/JSONUser
+gh repo clone keshavsoft/JSONUtility
+gh repo clone keshavsoft/JSONReports
