1bf8fe5b3cd63f83fedad0535f6097c2e463713c
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-05-30 18:54:10 +0530

    Update StartBatch.bat

--- StartBatch.bat
+++ StartBatch.bat
@@ -1,1 +1,8 @@
+gh repo clone keshavsoft/JSONDataSupply
 
+gh repo clone keshavsoft/AdminApi
+gh repo clone keshavsoft/KStaticFiles
+gh repo clone keshavsoft/JsonApi
+gh repo clone keshavsoft/JSONUser
+gh repo clone keshavsoft/JSONUtility
+gh repo clone keshavsoft/JSONReports
