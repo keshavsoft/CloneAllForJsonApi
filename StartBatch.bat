33b5e4bf1e88faf018617b67a3f1624caf6f0f3a
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-05-30 18:56:13 +0530

    Update StartBatch.bat

--- StartBatch.bat
+++ StartBatch.bat
@@ -14,3 +14,16 @@ xcopy AdminApi\Projects\JSONAdminApi JsonApi\Projects\JSONAdminApi /s /e /Y
 xcopy JSONReports\Projects\JSONReports JsonApi\Projects\JSONReports /s /e /Y
 xcopy JSONUser\Projects\JSONUser JsonApi\Projects\JSONUser /s /e /Y
 xcopy JSONUtility\Projects\JSONUtility JsonApi\Projects\JSONUtility /s /e /Y
+
+
+npm i
+
+
+cd KStaticFiles
+node start
+cd..
+
+
+xcopy JsonApi\KeshavSoft\KData JsonApi\KData /s /e /Y
+
+
