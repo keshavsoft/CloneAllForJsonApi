3ec72bf8021260e6313ac1d4e7904fac49db3e94
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-05-31 10:05:03 +0530

    Update StartBatch.bat

--- StartBatch.bat
+++ StartBatch.bat
@@ -8,12 +8,12 @@ gh repo clone keshavsoft/JSONUtility
 gh repo clone keshavsoft/JSONReports
 
 
-xcopy JSONDataSupply\DataSupply JsonApi\DataSupply /s /e /Y
+xcopy JSONDataSupply\DataSupply JsonApi\DataSupply\ /s /e /Y
 
-xcopy AdminApi\Projects\JSONAdminApi JsonApi\Projects\JSONAdminApi /s /e /Y
-xcopy JSONReports\Projects\JSONReports JsonApi\Projects\JSONReports /s /e /Y
-xcopy JSONUser\Projects\JSONUser JsonApi\Projects\JSONUser /s /e /Y
-xcopy JSONUtility\Projects\JSONUtility JsonApi\Projects\JSONUtility /s /e /Y
+xcopy AdminApi\Projects\JSONAdminApi JsonApi\Projects\JSONAdminApi\ /s /e /Y
+xcopy JSONReports\Projects\JSONReports JsonApi\Projects\JSONReports\ /s /e /Y
+xcopy JSONUser\Projects\JSONUser JsonApi\Projects\JSONUser\ /s /e /Y
+xcopy JSONUtility\Projects\JSONUtility JsonApi\Projects\JSONUtility\ /s /e /Y
 
 
 npm i
