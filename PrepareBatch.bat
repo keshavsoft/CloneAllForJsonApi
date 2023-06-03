849d5c411c679c1c9ac4e6e825d89e9ba97ba20b
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-06-03 20:42:25 +0530

    Update PrepareBatch.bat

--- PrepareBatch.bat
+++ PrepareBatch.bat
@@ -4,3 +4,4 @@ cd..
 
 
 xcopy StaticWithInclude\public\Projects\JSONAdminApi JsonApi\public\JSONAdminApi\  /s /e /Y
+xcopy StaticWithInclude\public\Projects\JSONUser JsonApi\public\JSONUser\  /s /e /Y
