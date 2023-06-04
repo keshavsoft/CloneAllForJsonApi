107b422030c47f397d68e2e26f8682cde00bc448
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-06-04 19:43:00 +0530

    Update StartBatch.sh

--- StartBatch.sh
+++ StartBatch.sh
@@ -1,6 +1,7 @@
 git clone -c core.longpaths=true https://github.com/keshavsoft/JSONDataSupply
 git clone -c core.longpaths=true https://github.com/keshavsoft/AdminApi
-git clone -c core.longpaths=true https://github.com/keshavsoft/AdminApi
-git clone -c core.longpaths=true https://github.com/keshavsoft/AdminApi
-git clone -c core.longpaths=true https://github.com/keshavsoft/AdminApi
-git clone -c core.longpaths=true https://github.com/keshavsoft/AdminApi
+git clone -c core.longpaths=true https://github.com/keshavsoft/StaticWithInclude
+git clone -c core.longpaths=true https://github.com/keshavsoft/JsonApi
+git clone -c core.longpaths=true https://github.com/keshavsoft/JSONUser
+git clone -c core.longpaths=true https://github.com/keshavsoft/JSONUtility
+git clone -c core.longpaths=true https://github.com/keshavsoft/JSONReports
