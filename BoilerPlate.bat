d0b7e5354df2d88455d84b34647f67a2a993a000
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-06-09 19:12:29 +0530

    Update BoilerPlate.bat

--- BoilerPlate.bat
+++ BoilerPlate.bat
@@ -14,7 +14,15 @@ xcopy JSONReports\Projects\JSONReports JsonApi\Projects\JSONReports\ /s /e /Y
 xcopy JSONUser\Projects\JSONUser JsonApi\Projects\JSONUser\ /s /e /Y
 xcopy JSONUtility\Projects\JSONUtility JsonApi\Projects\JSONUtility\ /s /e /Y
 
-
 npm i
 
 
+
+
+cd StaticWithInclude
+node start
+cd..
+
+
+xcopy StaticWithInclude\public\Projects\JSONAdminApi JsonApi\public\JSONAdminApi\  /s /e /Y
+xcopy StaticWithInclude\public\Projects\JSONUser JsonApi\public\JSONUser\  /s /e /Y
