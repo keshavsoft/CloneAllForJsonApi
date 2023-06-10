5cddc925ec534fe68cee5e739cf28cef967fea8e
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-06-10 15:18:22 +0530

    Update Test.bat

--- Test.bat
+++ Test.bat
@@ -2,6 +2,6 @@ if exist "JsonApi\KeshavSoft\BoilerPate\Empty\" (
     if exist "JsonApi\KData\" (
         echo Yes 
     ) else (
-        ROBOCOPY JsonApi\KeshavSoft\BoilerPate\Empty JsonApi\ /MIR
+        ROBOCOPY JsonApi\KeshavSoft\BoilerPate\Empty\KData JsonApi\KData /MIR
     )
 )
