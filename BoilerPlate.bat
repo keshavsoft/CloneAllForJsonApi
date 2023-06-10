3b24596b5770d821c9d90efaaec7b396ea0d4374
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-06-10 15:01:43 +0530

    Update BoilerPlate.bat

--- BoilerPlate.bat
+++ BoilerPlate.bat
@@ -26,3 +26,5 @@ cd..
 
 xcopy StaticWithInclude\public\Projects\JSONAdminApi JsonApi\public\JSONAdminApi\  /s /e /Y
 xcopy StaticWithInclude\public\Projects\JSONUser JsonApi\public\JSONUser\  /s /e /Y
+
+
