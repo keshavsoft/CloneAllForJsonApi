15c05c39663ef6bf9c2075d6881acfc6f2783fd5
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-06-21 15:53:48 +0530

    Create BoilerPlateOld.sh

---
+++ BoilerPlateOld.sh
@@ -0,0 +1,26 @@
+git clone -c core.longpaths=true https://github.com/keshavsoft/JSONDataSupply
+git clone -c core.longpaths=true https://github.com/keshavsoft/AdminApi
+git clone -c core.longpaths=true https://github.com/keshavsoft/StaticWithInclude
+
+git clone -c core.longpaths=true https://github.com/keshavsoft/JsonApi
+git clone -c core.longpaths=true https://github.com/keshavsoft/JSONUser
+git clone -c core.longpaths=true https://github.com/keshavsoft/JSONUtility
+git clone -c core.longpaths=true https://github.com/keshavsoft/JSONReports
+
+cp -r JSONDataSupply/DataSupply/. JsonApi/DataSupply
+
+cp -r AdminApi/Projects/JSONAdminApi/. JsonApi/Projects/JSONAdminApi
+cp -r JSONReports/Projects/JSONReports/. JsonApi/Projects/JSONReports
+cp -r JSONUser/Projects/JSONUser/. JsonApi/Projects/JSONUser
+cp -r JSONUtility/Projects/JSONUtility/. JsonApi/Projects/JSONUtility
+
+npm install
+
+cd StaticWithInclude
+node start
+cd -
+
+cp -r JSONDataSupply/DataSupply/. JsonApi/DataSupply
+
+cp -r StaticWithInclude/public/Projects/JSONAdminApi/. JsonApi/public/JSONAdminApi
+cp -r StaticWithInclude/public/Projects/JSONUser/. JsonApi/public/JSONUser
