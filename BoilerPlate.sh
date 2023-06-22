6b3b769d3e795a8916f1443aa037222c0811f0db
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-06-22 20:38:27 +0530

    Update BoilerPlate.sh

--- BoilerPlate.sh
+++ BoilerPlate.sh
@@ -29,6 +29,6 @@ cp -r StaticWithInclude/public/Projects/JSONUser/. JsonApi/public/JSONUser
 cp -r StaticWithInclude/public/Projects/JSONApi/. JsonApi/public/JSONApi
 cp -r StaticWithInclude/public/Projects/Garments/. JsonApi/public/Garments
 cp -r StaticWithInclude/public/Projects/JSONUtility/. JsonApi/public/JSONUtility
-
+cp -r StaticWithInclude/public/Projects/JSONReports/. JsonApi/public/JSONReports
 
 [ ! -d "$dldir" ] && cp -r JsonApi/KeshavSoft/BoilerPlate/Empty/KData/. "$dldir"
