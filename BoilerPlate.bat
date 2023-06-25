dc49ac3d507b1df58720aab80a2d49aa7ff88844
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-06-25 22:16:32 +0530

    Update BoilerPlate.bat

--- BoilerPlate.bat
+++ BoilerPlate.bat
@@ -6,6 +6,7 @@ git clone -c core.longpaths=true https://github.com/keshavsoft/JsonApi
 git clone -c core.longpaths=true https://github.com/keshavsoft/JSONUser
 git clone -c core.longpaths=true https://github.com/keshavsoft/JSONUtility
 git clone -c core.longpaths=true https://github.com/keshavsoft/JSONReports
+git clone -c core.longpaths=true https://github.com/keshavsoft/JSONImport
 
 ROBOCOPY JSONDataSupply\DataSupply JsonApi\DataSupply\ /MIR
 
@@ -13,6 +14,10 @@ xcopy AdminApi\Projects\JSONAdminApi JsonApi\Projects\JSONAdminApi\ /s /e /Y
 xcopy JSONReports\Projects\JSONReports JsonApi\Projects\JSONReports\ /s /e /Y
 xcopy JSONUser\Projects\JSONUser JsonApi\Projects\JSONUser\ /s /e /Y
 xcopy JSONUtility\Projects\JSONUtility JsonApi\Projects\JSONUtility\ /s /e /Y
+xcopy JSONImport\Projects\JSONImport JsonApi\Projects\JSONImport\ /s /e /Y
+xcopy JSONImport\Projects\Tally JsonApi\Projects\Tally\ /s /e /Y
+
+
 
 call npm install
 
