b9a29e2b24af45139ad0d19c5a177570adb21c98
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-10-31 17:23:03 +0530

    Update BoilerPlate.bat

--- BoilerPlate.bat
+++ BoilerPlate.bat
@@ -25,18 +25,11 @@ call npm install
 
 
 cd StaticWithInclude
-node start
+node ForJsonApi
 cd..
 
 
-xcopy StaticWithInclude\public\Projects\JSONAdminApi JsonApi\public\JSONAdminApi\  /s /e /Y
-xcopy StaticWithInclude\public\Projects\JSONUser JsonApi\public\JSONUser\  /s /e /Y
 xcopy StaticWithInclude\public\Projects\JSONApi  JsonApi\public\JSONApi\  /s /e /Y
-xcopy StaticWithInclude\public\Projects\Garments  JsonApi\public\Garments\  /s /e /Y
-xcopy StaticWithInclude\public\Projects\JSONUtility  JsonApi\public\JSONUtility\  /s /e /Y
-xcopy StaticWithInclude\public\Projects\JSONReports  JsonApi\public\JSONReports\  /s /e /Y
-xcopy StaticWithInclude\public\Projects\Cleaning  JsonApi\public\Cleaning\  /s /e /Y
-xcopy StaticWithInclude\public\Projects\ClientLogin  JsonApi\public\ClientLogin\  /s /e /Y
 
 
 if exist "JsonApi\KeshavSoft\BoilerPlate\Empty\" (
