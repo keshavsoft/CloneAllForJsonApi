4ae6a25d77229be1421d6ee51345d05d16a1af57
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-12-14 17:55:13 +0530

    Update Simple.bat

--- Simple.bat
+++ Simple.bat
@@ -1,13 +1,22 @@
 git clone -c core.longpaths=true https://github.com/keshavsoft/JSONDataSupply
+git clone -c core.longpaths=true https://github.com/keshavsoft/AdminApi
 
 git clone -c core.longpaths=true https://github.com/keshavsoft/JsonApi
 git clone -c core.longpaths=true https://github.com/keshavsoft/JSONUser
+git clone -c core.longpaths=true https://github.com/keshavsoft/JSONUtility
 git clone -c core.longpaths=true https://github.com/keshavsoft/JSONReports
+git clone -c core.longpaths=true https://github.com/keshavsoft/JSONImport
 
 ROBOCOPY JSONDataSupply\DataSupply JsonApi\DataSupply\ /MIR
 
-xcopy JSONUser\Projects\JSONUser JsonApi\Projects\JSONUser\ /s /e /Y
+xcopy AdminApi\Projects\JSONAdminApi JsonApi\Projects\JSONAdminApi\ /s /e /Y
 xcopy JSONReports\Projects\JSONReports JsonApi\Projects\JSONReports\ /s /e /Y
+xcopy JSONUser\Projects\JSONUser JsonApi\Projects\JSONUser\ /s /e /Y
+xcopy JSONUtility\Projects\JSONUtility JsonApi\Projects\JSONUtility\ /s /e /Y
+xcopy JSONImport\Projects\JSONImport JsonApi\Projects\JSONImport\ /s /e /Y
+xcopy JSONImport\Projects\Tally JsonApi\Projects\Tally\ /s /e /Y
+
+
 
 call npm install
 
